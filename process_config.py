366ecbf68b4a7d4ac6a815068548901d2afe5f6d
ahmed <ahmedakheirallah@gmail.com>
2026-05-16 22:26:47 +0400

    done

--- process_config.py
+++ process_config.py
@@ -1,12 +1,57 @@
-def process_config(path: str) -> dict:
+def process_confg(config: dict) -> dict:
+    process: dict = {}
+    check = "WIDTH"
+    try:
+        if int(config["WIDTH"]) <= 2:
+            raise Exception
+        else:
+            process["WIDTH"] = int(config["WIDTH"])
+
+        check = "HEIGHT"
+        if int(config["HEIGHT"]) <= 2:
+            raise Exception
+        else:
+            process["HEIGHT"] = int(config["HEIGHT"])
+
+        check = "ENTRY"
+        entry = config["ENTRY"].split(",")
+        entry[0], entry[1] = int(entry[0]), int(entry[1])
+        process["ENTRY"] = tuple(entry)
+
+        check = "EXIT"
+        exit_t = config["EXIT"].split(",")
+        exit_t[0], exit_t[1] = int(exit_t[0]), int(exit_t[1])
+        process["EXIT"] = tuple(exit_t)
+
+        check = "OUTPUT_FILE"
+        process["OUTPUT_FILE"] = config["OUTPUT_FILE"]
+
+        check = "PERFECT"
+        if config["PERFECT"] == "True":
+            process["PERFECT"] = True
+        elif config["PERFECT"] == "False":
+            process["PERFECT"] = False
+        else:
+            raise Exception
+
+        check = "SEED"
+        process["SEED"] = int(config["SEED"])
+    except Exception:
+        print(f"Invalid {check}")
+
+    return process
+
+
+def read_config(path: str) -> dict:
     '''Handles processing the config.txt file and edge cases'''
-    mandatory_keys = [
+    possible_keys = [
         "WIDTH",
         "HEIGHT",
         "ENTRY",
         "EXIT",
         "OUTPUT_FILE",
-        "PERFECT"
+        "PERFECT",
+        "SEED"
     ]
     try:
         config_dict: dict = {}
@@ -17,15 +62,21 @@ def process_config(path: str) -> dict:
                 if setting.count("=") != 1:
                     raise OSError("Config Format: 'KEY=VALUE'")
                 key, value = setting.split("=")
-                if key.upper() not in mandatory_keys:
+                if key.upper() not in possible_keys:
                     raise OSError(f"Unknown key: {key}")
                 config_dict[key] = value
-    except OSError as err:
+        if "SEED" not in config_dict.keys():
+            config_dict["SEED"] = '42'
+        if len(config_dict) < 7:
+            raise OSError("Missing configurations")
+    except Exception as err:
         print(err)
         return {}
 
+    config_dict = process_confg(config_dict)
     return config_dict
 
 
 if __name__ == "__main__":
-    res = process_config("confi.txt")
+    res = read_config("config.txt")
+    print(res)
